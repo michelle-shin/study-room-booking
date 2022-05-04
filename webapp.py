b5d73f86b1eec6f825d46d3ea9824d7611cc6623
goutamthukral <goutamthukral@gmail.com>
2022-05-04 10:35:37 -0700

    removed bugs regarding rendering html pages

--- webapp.py
+++ webapp.py
@@ -24,5 +24,45 @@ def homepage():
     except:
         return "", 400
 
+@app.route('/home', methods=["GET","POST"]) 
+def home(): 
+    with open('data/rooms.json') as fp:
+        data = json.load(fp)
+    BCIT = Rooms(data)
+    try:
+        booking = request.form.get('booking')
+        room_number = request.form.get('room')
+        if request.method == 'POST' and booking is not None:
+            room, time_slot = booking.split(',')
+            BCIT.get_room(room).book("A01283117", time_slot)
+            BCIT.save_to_json()
+            return render_template('home.html', methods=['GET','POST'], rooms=BCIT.get_room_numbers()), 200
+        elif request.method == 'POST' and room_number is not None:
+            return render_template('room.html', methods=['GET','POST'], room=room_number, availabilities=BCIT.get_room_availability(room_number)), 200
+        else:
+            return render_template('home.html', methods=['GET','POST'], rooms=BCIT.get_room_numbers()), 200
+    except:
+        return "", 400
+
+@app.route('/room', methods=["GET","POST"]) 
+def room(): 
+    with open('data/rooms.json') as fp:
+        data = json.load(fp)
+    BCIT = Rooms(data)
+    try:
+        booking = request.form.get('booking')
+        room_number = request.form.get('room')
+        if request.method == 'POST' and booking is not None:
+            room, time_slot = booking.split(',')
+            BCIT.get_room(room).book("A01283117", time_slot)
+            BCIT.save_to_json()
+            return render_template('home.html', methods=['GET','POST'], rooms=BCIT.get_room_numbers()), 200
+        elif request.method == 'POST' and room_number is not None:
+            return render_template('room.html', methods=['GET','POST'], room=room_number, availabilities=BCIT.get_room_availability(room_number)), 200
+        else:
+            return render_template('home.html', methods=['GET','POST'], rooms=BCIT.get_room_numbers()), 200
+    except:
+        return "", 400
+
 if __name__ == "__main__":
     app.run(debug=True)
