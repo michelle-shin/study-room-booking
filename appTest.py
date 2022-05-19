fb7a51baaddc9b1093d197e63400c913d3e813ac
goutamthukral <goutamthukral@gmail.com>
2022-05-18 22:14:16 -0700

    added unit-tests

--- appTest.py
+++ appTest.py
@@ -4,6 +4,6 @@ from models.credentials import Credentials
 from models.users import Users
 
 def test_get_time_slots():
-    path = '../data/sam.json'
+    path = 'data/sam.json'
     BCIT = Rooms(path)
     assert set(BCIT.get_time_slots()) == {"11:00 - 12:00", "15:00 - 16:00", "16:00 - 17:00"}
