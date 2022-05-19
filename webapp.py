7715d1c5ea70e9c014d4dc88391b2230913b1da8
goutamthukral <goutamthukral@gmail.com>
2022-05-19 03:22:11 -0700

    added Procfile

--- webapp.py
+++ webapp.py
@@ -193,4 +193,4 @@ def admin():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug=True, host="0.0.0.0",  port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
