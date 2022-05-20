4dae5d54443a8f863b888b8b0de5e4858ced2930
goutamthukral <goutamthukral@gmail.com>
2022-05-20 10:15:13 -0700

    cleaning up data after class demonstration

--- testing.py
+++ testing.py
@@ -6,12 +6,14 @@ from models.credentials import Credentials
 from cryptography.fernet import Fernet
 from models.users import Users
 from models.email import Email
+import requests
 
 def main():
 
-    mail = Email()
-    # mail.send_booking_confirmation("A01283117", "555", "8:00 - 9:00")
-    mail.send_password("A01283112")
+    url = "https://prod-15.canadacentral.logic.azure.com:443/workflows/d72fb653f1734f0d838a4b96e6239068/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VeSm7g3ly7vfi4Mja6hPyp1wj_Zy-tPxRQk1MY8y8rI"
+    obj = {"message":"hi from python2","email":"goutamthukral@gmail.com","subject":"Test"}
 
+    x = requests.post(url, json = obj)
+    print(x)
 if __name__ == "__main__":
     main()
